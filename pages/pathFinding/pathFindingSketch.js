a91e20103fc25ec8fa1b05909d6fad853f75612c
Tyler Hostler-Mathis <Hostler.Mathis.Tyler@gmail.com>
2019-12-27 14:16:36 -0500

    testing colors

--- pages/pathFinding/pathFindingSketch.js
+++ pages/pathFinding/pathFindingSketch.js
@@ -1,6 +1,25 @@
+let loc = 0;
+let cellSize = 2;
+let cellsX, cellsY;
+
 function setup() {
+
+  cellsX = windowWidth / cellSize;
+  cellsY = windowHeight / cellSize;
+  
+  noStroke();
+
+  createCanvas(windowWidth, windowHeight);
+
+  for (let i = 0; i < cellsX; i++)
+  {
+    for (let j = 0; j < cellsY; j++)
+    {
+      fill(i, j, 100);
+      ellipse(i * cellSize, j * cellSize, cellSize, cellSize);
+    }
+  }
 }
 
 function draw() {
-  ellipse(50, 50, 80, 80);
 }
